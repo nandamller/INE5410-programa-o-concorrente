4efa2e1739d7f907a474a1235bcf203ed3cd9681
Fernanda <mllernanda@gmail.com>
2023-05-13 09:01:59 -0300

    entrega semáforo

--- AFs/AF2.5-semaforo/AF-semaforo/exercicio_2/build/macos-semaphore-shim.d
+++
@@ -1,4 +0,0 @@
-build/macos-semaphore-shim.o: macos-semaphore-shim.c semaphore.h \
- /usr/include/semaphore.h
-semaphore.h:
-/usr/include/semaphore.h:
--- AFs/AF2.5-semaforo/AF-semaforo/exercicio_2/build/main.d
+++
@@ -1,3 +0,0 @@
-build/main.o: main.c semaphore.h /usr/include/semaphore.h
-semaphore.h:
-/usr/include/semaphore.h:
